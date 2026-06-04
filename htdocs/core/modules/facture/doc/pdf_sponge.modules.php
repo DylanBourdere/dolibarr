0f16cc5bb0c55f781547acb4ba26fc82a3d3714a
Dylan Bourdere <d.bourdere@gmail.com>
2026-06-04 09:17:06 +0200

    Fix situation invoice VAT summary in sponge PDF

--- htdocs/core/modules/facture/doc/pdf_sponge.modules.php
+++ htdocs/core/modules/facture/doc/pdf_sponge.modules.php
@@ -931,7 +931,7 @@ class pdf_sponge extends ModelePDFFactures
 					}
 					// Collecte des totaux par valeur de tva dans $this->tva["taux"]=total_tva
 					$prev_progress = $object->lines[$i]->get_prev_progress($object->id);
-					if ($prev_progress > 0 && !empty($object->lines[$i]->situation_percent)) { // Compute progress from previous situation
+					if (getDolGlobalInt('INVOICE_USE_SITUATION') == 1 && $prev_progress > 0 && !empty($object->lines[$i]->situation_percent)) { // Compute progress from previous situation
 						if (isModEnabled("multicurrency") && $object->multicurrency_tx != 1) {
 							$tvaligne = $sign * $object->lines[$i]->multicurrency_total_tva * ($object->lines[$i]->situation_percent - $prev_progress) / $object->lines[$i]->situation_percent;
 						} else {
